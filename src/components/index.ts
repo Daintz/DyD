756c75d9fa5148838a0ba589cad4432fb6035852
Daniel Lopez <felipelopez@carryt.co>
2025-06-02 00:29:32 -0500

    cambios

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,4 @@ export * from './ui/top-menu/TopMenu';
 export * from './ui/not-found/PageNotFound';
 export * from './ui/main-section/MainSection';
 export * from './ui/title/Title';
-export * from './ui/title-center/titleCenter';
+export * from './ui/title-center/TitleCenter';
