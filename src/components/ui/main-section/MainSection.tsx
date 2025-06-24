20fb088a4822a24170633882106c47e90f8daf83
Daniel Lopez <felipelopez@carryt.co>
2025-06-24 12:35:44 -0500

    Agregando mejoras a los textos de la interfaz

--- src/components/ui/main-section/MainSection.tsx
+++ src/components/ui/main-section/MainSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const MainSection = () => {
   return (
-    <div className="bg-white w-full h-90 relative">
+    <div className="bg-white w-full h-60 relative">
       <Image
         src="https://res.cloudinary.com/dtttwxbgr/image/upload/v1750742474/D_1000_x_300_mm_1000_x_250_mm_zi6m04.png"
         alt="Descripción de la imagen"
