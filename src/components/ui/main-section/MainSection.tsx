9c48e65a741d3d68b7339392aad2e5354b5491bc
Daniel Lopez <felipelopez@carryt.co>
2025-07-12 14:24:39 -0500

    Agregando imagenes optimizadas a webp

--- src/components/ui/main-section/MainSection.tsx
+++ src/components/ui/main-section/MainSection.tsx
@@ -5,7 +5,7 @@ export const MainSection = () => {
     <>
       <div className="relative w-[100%] h-[300px] lg:hidden">
         <Image
-          src="https://res.cloudinary.com/dtttwxbgr/image/upload/v1751082025/main-section-1000x600_mm_ye1umd.png"
+          src="https://res.cloudinary.com/dtttwxbgr/image/upload/v1752348295/D_1000_x_600_mm_n1bz8x.webp"
           alt="Logo D&D Gadgets Mobile"
           fill
           style={{ objectFit: "contain" }}
@@ -16,7 +16,7 @@ export const MainSection = () => {
       {/* Imagen para desktop */}
       <div className="relative w-[100%] h-[300px] hidden lg:block">
         <Image
-          src="https://res.cloudinary.com/dtttwxbgr/image/upload/v1751082025/main-section-1000x250_mm_xptd9r.png"
+          src="https://res.cloudinary.com/dtttwxbgr/image/upload/v1752348296/main-section-1000x250_mm_xptd9r_ago7ak.webp"
           alt="Logo D&D Gadgets Desktop"
           fill
           style={{ objectFit: "contain" }}
